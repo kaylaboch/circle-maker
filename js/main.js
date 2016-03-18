d6b65b71f982f007e1014bc77e583867e392f34c
kaylaboch <boch.kayla57@gmail.com>
2016-03-18 16:02:55 -0400

    added js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var $html = $('html');
+var $body = $('body');
+
+$html.on('keydown', function) (e) {
+  var div = $('<div>');
+  body.append(div);
+  div.addClass ('ball');
+  div.css('top' Math.random());
+}
