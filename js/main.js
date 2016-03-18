ecb2d66811a730925a7c6be8c17182d3f470436a
kaylaboch <boch.kayla57@gmail.com>
2016-03-18 16:08:29 -0400

    gap

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $html.on('keydown', function (e) {
   var div = $('<div>');
 
   $body.append(div);
-  div.addClass ('ball');
-  div.css ('top', Math.random() *1000);
+  div.addClass('ball');
+  div.css('top', Math.random() *1000);
   div.css ('left', Math.random() *1000);
 });
