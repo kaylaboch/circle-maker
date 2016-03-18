ed86574f83211e7f8e9fa224c98e520f65b02891
kaylaboch <boch.kayla57@gmail.com>
2016-03-18 16:04:38 -0400

    commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 var $html = $('html');
 var $body = $('body');
 
-$html.on('keydown', function) (e) {
+$html.on('keydown', function (e) {
   var div = $('<div>');
-  body.append(div);
+  $body.append(div);
   div.addClass ('ball');
-  div.css'top' Math.random();
+  div.css ('top', Math.random() *1000);
 }
