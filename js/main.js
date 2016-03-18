1cc276c7886a87bf5025267001351203d5316a51
kaylaboch <boch.kayla57@gmail.com>
2016-03-18 16:07:45 -0400

    enter

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ var $html = $('html');
 var $body = $('body');
 
 $html.on('keydown', function (e) {
-  var div = $('<div>' );
+  var div = $('<div>');
+  
   $body.append(div);
   div.addClass ('ball');
   div.css ('top', Math.random() *1000);
