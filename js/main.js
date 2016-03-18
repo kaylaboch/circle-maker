ed93610a6e8f989540af8505399a9f4272703177
kaylaboch <boch.kayla57@gmail.com>
2016-03-18 16:10:16 -0400

    spacing

--- js/main.js
+++ js/main.js
@@ -6,6 +6,6 @@ $html.on('keydown', function (e) {
 
   $body.append(div);
   div.addClass('ball');
-  div.css('top' , Math.random() *1000);
-  div.css('left' , Math.random() *1000);
+  div.css('top', Math.random() * 1000);
+  div.css('left', Math.random() * 1000);
 });
